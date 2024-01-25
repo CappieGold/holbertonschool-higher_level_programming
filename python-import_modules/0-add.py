d1594869259e223a717e62e9ff438a7c41ba7788
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-01-25 14:01:04 +0000

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import add_0
-    resultat = add_0.add(1, 2)
-    print("{} + {} = {}".format(1, 2, resultat))
+    a = 1
+    b = 2
+    resultat = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, resultat))
