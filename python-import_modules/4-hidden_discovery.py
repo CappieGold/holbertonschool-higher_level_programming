df20b46e7d60c0e05c6fb936d00aa3fdc9db5751
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-01-26 13:21:40 +0000

    task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
     import hidden_4
 
     names = dir(hidden_4)
-    filtered_names = [name for name in names if not name.startswith('__')]
-
-    for name in sorted(filtered_names):
-        print(name)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
