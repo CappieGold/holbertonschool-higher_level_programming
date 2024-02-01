4f1d867a0c84b59131e68559911e4de59805f3d6
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-02-01 16:13:17 +0000

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    if x <= 0 or not my_list:
+        print()
+        return 0
     try:
         for i in range(x):
             print(my_list[i], end="")
