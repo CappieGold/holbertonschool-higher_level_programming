97daf68ecdb7851f94368f8286810b3d5f7a7d1b
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-02-13 15:00:12 +0000

    task 5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class."""
+
+
+class BaseGeometry:
+    """BaseGeometry class for future geometric shapes.
+
+    This class is intended to be a base class for other geometric shapes.
+    Currently, it doesn't provide any specific functionalities or attributes.
+    """
+    def __init__(self):
+        """Initialize the BaseGeometry object.
+
+        The initializer of the BaseGeometry class is currently a placeholder,
+        as this class is designed to be a base class for other classes and
+        does not yet contain any attributes or methods.
+        """
+        pass
