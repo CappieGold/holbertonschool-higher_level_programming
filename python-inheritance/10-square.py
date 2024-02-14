6b9ac99873ece3432108d01f39a640842d8e2303
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-02-14 13:48:46 +0000

    task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inheriting from Rectangle.
+
+    Represents a square with equal width and height.
+    """
+
+    def __init__(self, size):
+        """Initialize a new Square instance.
+        Validates size using integer_validator from Rectangle class.
+        """
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
