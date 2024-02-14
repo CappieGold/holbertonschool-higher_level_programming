e232635154bb1d95b2529170623e849d68deea66
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-02-14 13:56:47 +0000

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module for class square"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inheriting from Rectangle.
+
+    Represents a square with equal width and height.
+    """
+
+    def __init__(self, size):
+        """Initialize a new Square instance.
+        Validates size using integer_validator from Rectangle class.
+        """
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
