a55551f2b51842e95e2804a8f4abb68504003c04
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-02-15 13:07:55 +0000

    task 0 pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-"""read content of file with: with, r and utf 8"""
+""" Module for reading file contents. """
 
 
 def read_file(filename=""):
+    """ Reads and prints the contents of a file.
+
+    Args:
+        filename (str): The name of the file to be read.
+    """
     with open('my_file_0.txt', 'r', encoding="utf-8") as f:
         content = f.read()
         print(content, end='')
