990487dba2b1344c2612b8a63425a827ffcda6e9
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-02-15 13:31:27 +0000

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+""" Module for writing text to a file. """
+
+
 def write_file(filename="", text=""):
+    """ Writes a string to a text file and returns its length.
+
+    Args:
+        filename (str): The name of the file to write to.
+        text (str): The text to write into the file.
+
+    Returns:
+        int: The length of the text written.
+    """
     with open(filename, 'w') as file:
         file.write(text)
     return len(text)
