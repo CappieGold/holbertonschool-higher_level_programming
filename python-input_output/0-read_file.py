afc7c3f55b0298e348bc8b1e8831cc6ca0f4d8af
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-02-15 13:05:32 +0000

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""read content of file with: with, r and utf 8"""
+
+
 def read_file(filename=""):
     with open('my_file_0.txt', 'r', encoding="utf-8") as f:
         content = f.read()
