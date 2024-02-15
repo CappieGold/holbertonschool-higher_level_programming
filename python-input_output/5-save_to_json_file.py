ed3e920d066b969ed0720988e5b4336a0c32a17f
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-02-15 15:10:40 +0000

    task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Save object to a file as JSON string. """
 
+import json
+
 
 def save_to_json_file(my_obj, filename):
     """ Save a Python object to a file in JSON format.
@@ -9,7 +11,6 @@ def save_to_json_file(my_obj, filename):
         my_obj: The Python object to serialize.
         filename (str): The file to write the JSON string to.
     """
-    import json
     content_write = json.dumps(my_obj)
     with open(filename, 'w', encoding="utf-8") as file:
         file.write(content_write)
