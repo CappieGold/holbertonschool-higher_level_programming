c83ad78ba70044170eb7070cd06116558247e2eb
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-02-18 18:41:29 +0000

    task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Defines the Student class."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name.
+            last_name (str): The last name.
+            age (int): The age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Dictionary representation of the Student.
+
+        Args:
+            attrs (list): Attributes to retrieve.
+
+        Returns:
+            dict: The dictionary representation.
+        """
+        if (isinstance(attrs, list) and
+                all(isinstance(attr, str) for attr in attrs)):
+            return {a: getattr(self, a) for a in attrs if hasattr(self, a)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student instance.
+
+        Args:
+            json (dict): A dictionary representation of the student.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
