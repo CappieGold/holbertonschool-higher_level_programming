c4d054a4e2944e4f429c8c69d51178e2ba0a5534
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-02-19 11:24:46 +0000

    task 1, create clase Base and empty __init_.py

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+class Base:
+    """ Base class for future models. Manages id assignment. """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialize a new instance of Base.
+
+        Args:
+            id (int, optional): The id of the object. If not provided, it's
+            automatically assigned based on __nb_objects.
+
+        Attributes:
+            id (int): The public id of the object.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
