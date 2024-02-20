30acc26c4e00dc4a285d9fc074a0348a994aa8ef
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-02-20 14:49:55 +0000

    task 6 __str__ method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from models.base import Base
 
+
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
@@ -63,3 +64,7 @@ class Rectangle(Base):
     def display(self):
         for _ in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}"\
+            .format(self.id, self.__x, self.__y, self.__width, self.__height)
