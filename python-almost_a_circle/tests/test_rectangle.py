70cd0fb4e2f93cb8cbfd95ccc956503729a2c84c
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-02-21 11:45:21 +0000

    add test for kwargs

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -113,6 +113,24 @@ class TestRectangle(unittest.TestCase):
         r1.update(89, 2, 3, 4, 5)
         self.assertEqual(str(r1), "[Rectangle] (89) 4/5 - 2/3")
 
+    def test_update_kwargs_only(self):
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(height=1)
+        self.assertEqual(str(r1), "[Rectangle] (1) 10/10 - 10/1")
+
+        r1.update(width=1, x=2)
+        self.assertEqual(str(r1), "[Rectangle] (1) 2/10 - 1/1")
+
+        r1.update(y=1, width=2, x=3, id=89)
+        self.assertEqual(str(r1), "[Rectangle] (89) 3/1 - 2/1")
+
+        r1.update(x=1, height=2, y=3, width=4)
+        self.assertEqual(str(r1), "[Rectangle] (89) 1/3 - 4/2")
+
+    def test_update_kwargs(self):
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(height=1, width=2, y=3, x=4, id=88)
+        self.assertEqual(str(r1), "[Rectangle] (88) 4/3 - 2/1")
 
 if __name__ == "__main__":
     unittest.main()
