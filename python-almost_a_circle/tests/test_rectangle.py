83bf44676766cbb2d5eacdd118640c1f21e06009
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-02-21 10:24:12 +0000

    add test for update attributes

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -96,6 +96,23 @@ class TestRectangle(unittest.TestCase):
         expected_output = " ###\n ###\n"
         self.assertEqual(s, expected_output)
 
+    def test_update(self):
+        r1 = Rectangle(10, 10, 10, 10)
+        r1.update(89)
+        self.assertEqual(str(r1), "[Rectangle] (89) 10/10 - 10/10")
+
+        r1.update(89, 2)
+        self.assertEqual(str(r1), "[Rectangle] (89) 10/10 - 2/10")
+
+        r1.update(89, 2, 3)
+        self.assertEqual(str(r1), "[Rectangle] (89) 10/10 - 2/3")
+
+        r1.update(89, 2, 3, 4)
+        self.assertEqual(str(r1), "[Rectangle] (89) 4/10 - 2/3")
+
+        r1.update(89, 2, 3, 4, 5)
+        self.assertEqual(str(r1), "[Rectangle] (89) 4/5 - 2/3")
+
 
 if __name__ == "__main__":
     unittest.main()
