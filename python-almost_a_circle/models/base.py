1e9c807f69258f55ca05829650e38af471014b07
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-02-22 14:30:19 +0000

    add json string to file, Save a list of Base instances to a JSON file.

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -33,3 +33,18 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save a list of Base instances to a JSON file.
+
+        Args:
+            list_objs (list): A list of inherited instances from Base.
+        """
+        filename = cls.__name__ + ".json"
+        list_dicts = []
+        if list_objs is not None:
+            list_dicts = [obj.to_dictionary() for obj in list_objs]
+        json_string = cls.to_json_string(list_dicts)
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(json_string)
