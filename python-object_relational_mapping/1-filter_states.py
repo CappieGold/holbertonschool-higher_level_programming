512ff48d4d98e1565183321f74b1530018df7fb5
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-03-21 15:45:13 +0000

    task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT * FROM states\
     WHERE name LIKE BINARY 'N%'\
-    ORDER BY id")
+    ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
